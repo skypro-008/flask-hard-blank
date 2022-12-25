5d971354b72e5f80acfa10d829bf0ddab99bc218
MG <mr.zizikk@gmail.com>
2022-12-25 17:09:58 +0300

    create models movie genre director dao/model/..., connect db

---
+++ dao/model/genre.py
@@ -0,0 +1,15 @@
+from marshmallow import Schema, fields
+
+from setup_db import db
+
+
+class Genre(db.Model):
+    __tablename__ = "director"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(155))
+
+
+class GenreSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
---
+++ dao/model/movie.py
@@ -0,0 +1,33 @@
+from marshmallow import Schema, fields
+
+from setup_db import db
+
+
+class Book(db.Model):
+    __tablename__ = "book"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(155))
+    description = db.Column(db.String(155))
+    trailer = db.Column(db.String)
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Integer)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'))
+    director_id = db.Column(db.Integer, db.ForeignKey('director.id'))
+
+    genre = db.relationship("Genre")
+    director = db.relationship("Director")
+
+
+class BookSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Int()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+    genre = fields.Str()
+    director = fields.Str()
